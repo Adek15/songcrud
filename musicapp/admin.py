4253a8cf448d4e9f8d55ebc61effb2a839124716
Adek15 <88534016+Adek15@users.noreply.github.com>
2022-10-28 10:24:59 +0100

    Created models

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import artiste_id, song_id, Lyric
 # Register your models here.
+admin.site.register(artiste_id)
+admin.site.register(song_id)
+admin.site.register(Lyric)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class artiste_id(models.Model):
+    first_name = models.CharField(max_length= 40)
+    last_name = models.CharField(max_length= 40)
+    age = models.IntegerField()
+
+class song_id(models.Model):
+    artiste_id = models.ForeignKey(artiste_id, on_delete = models.CASCADE)
+    title = models.CharField(max_length= 400)
+    date_released = models.DateField(default= datetime.today)
+    likes = models.IntegerField()
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(song_id, on_delete= models.CASCADE)
+    content = models.CharField(max_length= 1000000)
+    
